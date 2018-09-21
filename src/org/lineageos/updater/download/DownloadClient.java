a30cfa45e4fce89bb4e64ce27c819a3e5a366b44
Tad <tad@spotco.us>
2018-09-20 21:44:53 -0400

    Add support for routing over Tor
    Change-Id: Ibfe080c3d801af34fb64fda1b6b8f4f39a2b1ccf

--- src/org/lineageos/updater/download/DownloadClient.java
+++ src/org/lineageos/updater/download/DownloadClient.java
@@ -60,6 +60,7 @@ public interface DownloadClient {
         private DownloadClient.DownloadCallback mCallback;
         private DownloadClient.ProgressListener mProgressListener;
         private boolean mUseDuplicateLinks;
+        private boolean mOnionRouting;
 
         public DownloadClient build() throws IOException {
             if (mUrl == null) {
@@ -70,7 +71,7 @@ public interface DownloadClient {
                 throw new IllegalStateException("No download callback defined");
             }
             return new HttpURLConnectionClient(mUrl, mDestination, mProgressListener, mCallback,
-                    mUseDuplicateLinks);
+                    mUseDuplicateLinks, mOnionRouting);
         }
 
         public Builder setUrl(String url) {
@@ -95,6 +96,11 @@ public interface DownloadClient {
 
         public Builder setUseDuplicateLinks(boolean useDuplicateLinks) {
             mUseDuplicateLinks = useDuplicateLinks;
+            return this;
+        }
+
+        public Builder setUseOnionRouting(boolean onionRouting) {
+            mOnionRouting = onionRouting;
             return this;
         }
     }
