17cca5e4c2974bca0cbd498ecface75022c7c126
Tad <tad@spotco.us>
2018-09-20 21:44:53 -0400

    Add support for routing over Tor
    Change-Id: Ibfe080c3d801af34fb64fda1b6b8f4f39a2b1ccf

--- src/org/lineageos/updater/controller/UpdaterController.java
+++ src/org/lineageos/updater/controller/UpdaterController.java
@@ -358,12 +358,16 @@ public class UpdaterController {
         update.setFile(destination);
         DownloadClient downloadClient;
         try {
+            if(Utils.isOnionRoutingEnabled(mContext)) {
+                Utils.requestStartOrbot(mContext);
+            }
             downloadClient = new DownloadClient.Builder()
                     .setUrl(update.getDownloadUrl())
                     .setDestination(update.getFile())
                     .setDownloadCallback(getDownloadCallback(downloadId))
                     .setProgressListener(getProgressListener(downloadId))
                     .setUseDuplicateLinks(true)
+                    .setUseOnionRouting(Utils.isOnionRoutingEnabled(mContext))
                     .build();
         } catch (IOException exception) {
             Log.e(TAG, "Could not build download client");
@@ -398,6 +402,9 @@ public class UpdaterController {
             verifyUpdateAsync(downloadId);
             notifyUpdateChange(downloadId);
         } else {
+            if(Utils.isOnionRoutingEnabled(mContext)) {
+                Utils.requestStartOrbot(mContext);
+            }
             DownloadClient downloadClient;
             try {
                 downloadClient = new DownloadClient.Builder()
@@ -406,6 +413,7 @@ public class UpdaterController {
                         .setDownloadCallback(getDownloadCallback(downloadId))
                         .setProgressListener(getProgressListener(downloadId))
                         .setUseDuplicateLinks(true)
+                        .setUseOnionRouting(Utils.isOnionRoutingEnabled(mContext))
                         .build();
             } catch (IOException exception) {
                 Log.e(TAG, "Could not build download client");
